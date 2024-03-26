f16d7b32173c5592d3d73d30fcc4cd2418ead740
Lauri Ojansivu <x@xet7.org>
2024-03-27 01:34:55 +0200

    Added beginnings of build.sh script.

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This will build coolwallet.com executeable.
+# This is In Progress. More code will be added soon.
+#
+# When running executeable, database wil be added to same directory,
+# if it does not exist yet.
+
+if command -v apt &> /dev/null
+then
+    echo "apt found"
+    exit 1
+fi
+
+if command -v dnf &> /dev/null
+then
+    echo "dnf found"
+    exit 1
+fi
+
+if command -v brew &> /dev/null
+then
+    echo "brew found"
+    exit 1
+fi
+
